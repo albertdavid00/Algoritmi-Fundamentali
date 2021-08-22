e06bd5a15ec9d07e9a6b55302ac116f1f0a3a0bc
albertdavid00 <d.albert015@yahoo.com>
2021-08-22 16:54:08 +0300

    second best mst

--- AlgoritmiFundamentali/Labs/Lab3/second-best-mst.py
+++ AlgoritmiFundamentali/Labs/Lab3/second-best-mst.py
@@ -1,5 +1,9 @@
+import copy
+import math
+
+
 def getListAndEdges(oriented: bool = False):
-    f = open("grafpond.in", "r")
+    f = open("sndbest.in", "r")
     nrOfNodes, nrOfEdges = [int(x) for x in f.readline().split()]
     adjacentList = [[] for _ in range(nrOfNodes)]
     edges = []
@@ -54,7 +58,7 @@ def printMST(mst):
 
 
 def dfSearch(root):
-    global n, visited, adjTree, finalNode, dfPath, check, parentTree
+    global n, visited, adjTree, dfPath, check, parentTree
     if check == True:
         return
     visited[root] = 1
@@ -73,6 +77,17 @@ def dfSearch(root):
             check = True
             return
 
+def getMaxEdge(dfPath, newEdge):
+
+    maxEdge = [-1, -1, -1]
+    for edge in dfPath:
+        edges = (edge[0], edge[1])
+        if edge[2] > maxEdge[2]:
+            if newEdge[0] in edges and newEdge[1] in edges:
+                continue
+            maxEdge = edge
+
+    return maxEdge
 
 if __name__ == "__main__":
     oriented = False
@@ -86,7 +101,7 @@ if __name__ == "__main__":
     countEdgesInMST = 0
 
     edges.sort(key=lambda e: e[2])
-
+    remainingEdges = copy.deepcopy(edges)
     for edge in edges:
         fstNode, sndNode = edge[0], edge[1]
 
@@ -94,17 +109,14 @@ if __name__ == "__main__":
             unite(fstNode, sndNode)
             minimumSpanningTree.append(edge)
             countEdgesInMST += 1
+            remainingEdges.remove(edge)
 
         if countEdgesInMST == n - 1:  # MST has n-1 edges
             break
+    print("Best Tree")
+    printMST(minimumSpanningTree)
 
-    printMST(minimumSpanningTree)
-    # subp. a) and b)
     adjTree = [[] for _ in range(n)]
-    newEdge = (3, 5, 4)
-    newEdge = (newEdge[0] - 1, newEdge[1] - 1, newEdge[2])
-    minimumSpanningTree.append((newEdge[0], newEdge[1], newEdge[2]))
-    printMST(minimumSpanningTree)
     for edge in minimumSpanningTree:
         node1, node2, weight = edge[0], edge[1], edge[2]
         adjTree[node1].append((node2, weight))
@@ -112,24 +124,41 @@ if __name__ == "__main__":
         if not oriented:
             adjTree[node2].append((node1, weight))
 
-    for node in range(len(adjTree)):
-        print(node + 1, ":", [(node + 1, weight) for node, weight in adjTree[node]])
-
-    startNode, finalNode = newEdge[0], newEdge[1]
-    parentTree = [-1 for _ in range(n)]
-    visited = [0 for _ in range(n)]
-    dfPath = []
-    check = False
-
-    dfSearch(startNode)
-
-    maxWeightEdge = list(filter(lambda e: e[2] == max(map(lambda e: e[2], dfPath)), dfPath))
-    print("Max weighted edge in cycle:", (maxWeightEdge[0][0] + 1, maxWeightEdge[0][1] + 1, maxWeightEdge[0][2]))
-    print("Cycle:", [(node1 + 1, node2 + 1, weight) for (node1, node2, weight) in dfPath])
-
-    if maxWeightEdge[0][2] == newEdge[2]:
-        print("New graph obtained by adding the new edge doesn't have a mst-weight smaller then the original")
-    else:
-        print("New graph obtained by adding the new edge does have a mst-weight smaller then the original")
 
 
+    lastEdgeIndex = edges.index(minimumSpanningTree[-1])
+    sndBestList = [None, None, math.inf] # newEdge, edgeToBeRemovedFromMST, weightDifference
+
+    for i in range(len(remainingEdges)):
+        parentTree = [-1 for _ in range(n)]
+        visited = [0 for _ in range(n)]
+        dfPath = []
+        check = False
+        newEdge = remainingEdges[i]
+        node1, node2, weight = newEdge[0], newEdge[1], newEdge[2]
+        minimumSpanningTree.append(newEdge)
+        adjTree[node1].append([node2, weight])
+        if not oriented:
+            adjTree[node2].append([node1, weight])
+
+        dfSearch(node1)
+        maxEdge = getMaxEdge(dfPath, newEdge)
+        weightDifference = newEdge[2] - maxEdge[2]
+        minWeightDifference = sndBestList[2]
+
+        if weightDifference < minWeightDifference:
+            sndBestList[2] = weightDifference
+            sndBestList[0] = newEdge
+            sndBestList[1] = maxEdge
+
+        minimumSpanningTree.pop()
+        adjTree[node1].pop()
+        if not oriented:
+            adjTree[node2].pop()
+
+    secondBest = copy.deepcopy(minimumSpanningTree)
+
+    secondBest.append(sndBestList[0])
+    secondBest.remove(sndBestList[1])
+    print("Second Best Tree:")
+    printMST(secondBest)
